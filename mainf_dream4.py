fe301dcaa6750f212aada36ce9ababc347339c86
Atticusl <75166470+Atticusl@users.noreply.github.com>
2025-03-17 14:46:21 +0800

    Update mainf_dream4.py

--- mainf_dream4.py
+++ mainf_dream4.py
@@ -12,7 +12,7 @@ st = dataset['st']
 for i in range(5):
     maxdelay=5; E=3; tau=1; th=0.7
     start = time.time()
-    ic, td, fc = InVaXMap.InVaXMap(G = data[:,:,i], maxdelay = maxdelay, E = E, tau = tau, th = th)
+    ic, td, fc, _ = InVaXMap.InVaXMap(G = data[:,:,i], maxdelay = maxdelay, E = E, tau = tau, th = th)
     print(InVaXMap.eva_cri(fc,st[:,:,i])[0])
     end = time.time()
     print("time cost: {:.3f}s\n".format(end-start))
